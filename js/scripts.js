5f44afb714e902700562308b27dae6b6931ea3d5
Kathryn Carr and Lizzie Koehler <kathrynelizabethcarr@gmail.com>
2015-02-16 10:17:25 -0800

    adds click functionality to display entire address

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,18 @@ $(document).ready(function(){
     var inputtedAddress = $("#address").val();
     var contact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress};
 
-    $("#name-list").append("<li>" + contact.firstName + " " + contact.lastName + "</li>");
+    $("#name-list").append("<li><span class = 'contact'>" + contact.firstName + " " + contact.lastName + "</span></li>");
+
+
+    $(".contact").last().click(function() {
+
+
+      $("#show-contact").show();
+      $("#show-contact h3").text(contact.firstName + " " + contact.lastName);
+      $('.first-name').text(contact.firstName);
+      $('.last-name').text(contact.lastName);
+      $('.address').text(contact.address);
+    });
 
 
     event.preventDefault();
