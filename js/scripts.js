9fe246fbf9a1cccf22e57c1dc1548890fc914f92
Kathryn Carr and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-16 09:51:16 -0800

    Add base html to index and some JQuery

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$document.ready(function(){
+  ("#form").submit(function(event) {
+    var inputtedfirstName = $("#first-name").val();
+    var inputtedlastName = $("#last-name").val();
+    var inputtedaddress = $("#address").val();
+
+    var contacts = {firstName: };
+
+    event.preventDefault();
+
+  });
+});
